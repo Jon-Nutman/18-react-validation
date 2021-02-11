9799717adc29449c164da5bef658badaa014b3ff
Jon-Nutman <jonnut730@gmail.com>
2021-02-11 10:51:34 -0500

    doneish

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import validator from 'validator'
 import './App.css';
@@ -11,6 +10,8 @@ function App() {
   const [passwordError, setPasswordError] = useState('')
   const [confirmPasswordError, setConfirmPasswordError] = useState('')
   const [websiteError, setWebsiteError] = useState('')
+
+  const [formCompleted, setFormCompleted] = useState('')
 //INPUTS
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
@@ -19,42 +20,50 @@ function App() {
   const [confirmPassword, setConfirmPassword] = useState('')
   const [website, setWebsite] = useState('')
 
-
-
-
+  //FUNCTION TO CHECK FOR ERRORS ON SUMBIT
   function handleSubmit(e){ 
     let emailValidator = validator.isEmail(email)
     let urlValidator = validator.isURL(website)
     e.preventDefault()
-    console.log(validator)
     name === '' ? setNameError('Cannot be blank') : setNameError('')
     username === '' ? setUsernameError('Cannot be blank') : setUsernameError('')
     password === '' ? setPasswordError('Cannot be blank') : setPasswordError('')
     confirmPassword !== password ? setConfirmPasswordError('Passwords do not match') :setConfirmPasswordError('')
-    emailValidator == false ? setEmailError('Invalid email') : setEmailError('')
-    urlValidator == false ? setWebsiteError('Invalid url') : setWebsiteError('')
+    emailValidator === false ? setEmailError('Invalid email') : setEmailError('')
+    urlValidator === false ? setWebsiteError('Invalid url') : setWebsiteError('')
+
+    if (nameError === '' && emailError === '' && usernameError === '' && passwordError === '' && confirmPasswordError === '' && websiteError === '' ){
+    setFormCompleted('Thank You')
+       }else{
+       setFormCompleted('')
+       }
   }
+  
   return (
-    <div>
+    <div id='theWholeShabang'>
      <form onSubmit={handleSubmit}> 
 
+
+     <h1 className={formCompleted !== '' ? 'form-complete' : 'form-not-complete'} > {formCompleted}</h1>
+     <h1 className='text-label' >Proflie Form - All fields required</h1>
+
        <label id='text-position' className={nameError !== ''? 'text-label-red' : 'text-label'} htmlFor='name'>Name: {nameError}</label>
-       <input className={nameError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setName(e.target.value)}/>
+       <input placeholder=" Enter Name Here" className={nameError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setName(e.target.value)}/>
 
        <label id='text-position' className={emailError !== ''? 'text-label-red' : 'text-label'} htmlFor='email'>Email: {emailError}</label>
-       <input className={emailError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setEmail(e.target.value)}/>
+       <input placeholder=" Enter Email Here" className={emailError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setEmail(e.target.value)}/>
 
        <label id='text-position' className={usernameError !== ''? 'text-label-red' : 'text-label'} htmlFor='username'>Username: {usernameError}</label>
-       <input className={usernameError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setUsername(e.target.value)}/>
+       <input placeholder=" Enter Username Here" className={usernameError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setUsername(e.target.value)}/>
        
        <label id='text-position' className={passwordError !== ''? 'text-label-red' : 'text-label'} htmlFor='password'>Password: {passwordError}</label>
-       <input className={passwordError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setPassword(e.target.value)}/>
+       <input type='password' placeholder=" Enter Password Here" className={passwordError !== ''? 'input-box' : 'text-label'} onChange={(e) => setPassword(e.target.value)}/>
 
        <label id='text-position' className={confirmPasswordError !== ''? 'text-label-red' : 'text-label'} htmlFor='confirmPassword'>Confirm password:{confirmPasswordError}</label>
-       <input className={confirmPasswordError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setConfirmPassword(e.target.value)}/>
+       <input type='password' placeholder="Confirm Password Here" className={confirmPasswordError !== ''? 'input-box' : 'text-label'} onChange={(e) => setConfirmPassword(e.target.value)}/>
 
        <label id='text-position' className={websiteError !== ''? 'text-label-red' : 'text-label'} htmlFor='website'>Website: {websiteError}</label>
-       <input className={websiteError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setWebsite(e.target.value)}/>
+       <input placeholder=" Enter Website Here" className={websiteError !== ''? 'input-box' : 'text-label'} type="text" onChange={(e) => setWebsite(e.target.value)}/>
        
        <button type="submit">Submit</button>
      </form>
